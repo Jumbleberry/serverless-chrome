6d7e5da30fa14c198a41afe8401e05486f83eb3c
Hao <linghaoo@gmail.com>
2017-08-22 16:43:05 -0400

    Return failure immediately

--- src/handler.js
+++ src/handler.js
@@ -5,23 +5,22 @@ import { log, generateError } from './utils'
 // eslint-disable-next-line import/prefer-default-export
 export async function run (event, context, callback, handler = config.handler) {
   let handlerResult = {}
-  let handlerError = null
 
   try {
     await spawnChrome()
   } catch (error) {
     console.error('Error in spawning Chrome')
-    handlerError = error
+    return callback(error)
   }
 
   try {
     handlerResult = await handler(event, context)
   } catch (error) {
     console.error('Error in handler:', error)
-    handlerError = generateError(event)
+    return callback(generateError(event))
   }
 
   log('Handler result:', JSON.stringify(handlerResult, null, ' '))
 
-  return callback(handlerError, handlerResult)
+  return callback(null, handlerResult)
 }
