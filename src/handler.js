26a46c0e7a52ca40d58d9a3983497bad7abe7072
Hao <linghaoo@gmail.com>
2017-08-23 17:49:31 -0400

    Output request on success

--- src/handler.js
+++ src/handler.js
@@ -5,20 +5,26 @@ import { log, generateError } from './utils'
 // eslint-disable-next-line import/prefer-default-export
 export function run (event, context, callback, handler = config.handler) {
   asyncRun(event, context, callback, handler)
-    .catch(e => { log(e); callback(generateError(event, 'Error in firing pixel in headless chrome')) })
-    .then(v => { callback(null, v) })
+    .catch(e => { callback(generateError(event, 'Error in firing pixel in headless chrome')) })
+    .then(v => { callback(null, "Success") })
 }
 
 async function asyncRun (event, context, callback, handler) {
+  let handlerResult = {}
+
   try {
     await spawnChrome()
   } catch (error) {
+    log(error)
     throw new Error('Error in spawning Chrome')
   }
 
   try {
-    await handler(event, context)
+    handlerResult = await handler(event, context)
   } catch (error) {
+    log(error)
     throw new Error('Error in handler')
   }
+
+  log('Handler result:', JSON.stringify(handlerResult, null, ' '))
 }
