34a5e0c5037b67ac274adb3bb08815bcf5302c66
Hao <linghaoo@gmail.com>
2017-08-24 15:50:51 -0400

    Use context.fail(err) rather than calback(err)

--- src/handler.js
+++ src/handler.js
@@ -3,47 +3,26 @@ import { spawn as spawnChrome } from './chrome'
 import { log, generateError } from './utils'
 
 // eslint-disable-next-line import/prefer-default-export
-export function run (event, context, callback, handler = config.handler) {
-  // let handlerResult = {}
+export async function run (event, context, callback, handler = config.handler) {
+  var error = null
 
-  // spawnChrome().then(() => {
-  //   try {
-  //     handlerResult = handler(event, context)
-  //   } catch(e) {
-  //     log(e)
-  //     callback(generateError(event, 'Error in firing pixel in headless chrome'))
-  //   }
-  //   log('Handler result:', JSON.stringify(handlerResult, null, ' '))
-  //   log('All done!')
-  //   callback(null, 'Success')
-  // }).catch((err) => {
-  //   log('Error in spawning headless chrome:', err)
-  //   callback(generateError(event, 'Error in spawning headless chrome'))
-  // })
+  try {
+    await spawnChrome()
+  } catch(err) {
+    log('Error in spawning:', err)
+    error = generateError(event, 'Error in spawning')
+  }
 
-  spawnChrome().then(() => {
-    handler(event, context).then(() => {
-      return callback(null, 'Success')
-    })
-    .catch((e) => {
-      log('Error in handler:', e)
-      return callback(generateError(event, 'Error in handler'))
-    })
-  })
-  .catch((e) => {
-    log('Error in spawning chrome:', e)
-    return callback(generateError(event, 'Error in spawning'))
-  })
+  try {
+    await handler(event, context)
+  } catch(err) {
+    log('Error in handler:', err)
+    error = generateError(event, 'Error in handler')
+  }
 
-  // spawnChrome().then(function() {
-  //   return handler(event, context)
-  // }).then(function() {
-  //   log('Handler Success')
-  // }).catch(function(err) {
-  //   log('An error occurred:', err)
-  //   callback(generateError(event, 'Error in firing pixel in headless chrome'))
-  // }).then(function() {
-  //   log('All done!')
-  //   callback(null, 'Success')
-  // })
+  if (error) {
+    context.fail(error)
+  } else {
+    callback(null, 'Success')
+  }
 }
