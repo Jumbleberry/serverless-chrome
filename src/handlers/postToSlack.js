b1693c1b26f17aa20bdfbb86dcd604734cf2fc25
Hao <linghaoo@gmail.com>
2017-08-29 11:17:01 -0400

    Process every event

--- src/handlers/postToSlack.js
+++ src/handlers/postToSlack.js
@@ -1,54 +1,78 @@
 import config from '../config'
+import { log } from '../utils'
 
 export default (function postToSlackHandler (event) {
     var https = require('https');
 
-    var eventName = event['Records'][0]['eventName'];
-    var hid = event['Records'][0]['dynamodb']['NewImage']['hid']['N'];
-    if (eventName == 'INSERT') {
-        var msg = 'Pixel with hid:' + hid + ' failed to fire.';
-    }
+    event.Records.forEach(record => {
+        var eventId = record.eventID
+        var eventName = record.eventName
+        var data = record.dynamodb
+        log('Event ID: ' + eventId)
+        log('Event name: ' + eventName)
+        log('Dynamodb Record: %j', data)
 
-    // form data
-    var postData = JSON.stringify({
-        channel: config.slackWebHookChannel,
-        username: 'Angry Ian',
-        text: msg,
-        icon_emoji: ':scream:'
-    });
+        var msg = null;
+        var hid = null;
+        var userName = null;
+        var iconEmoji = null;
 
-    // request option
-    var options = {
-        host: config.slackWebHookHost,
-        port: 443,
-        method: 'POST',
-        path: config.slackWebHookPath,
-        headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData)
+        if (eventName == 'INSERT') {
+            msg = 'Pixel with hid: `' + hid + '` failed to fire.';
+            hid = data['NewImage']['hid']['N'];
+            userName = 'Angry Bot';
+            iconEmoji = ':rage:';
+        } else if (eventName == 'REMOVE') {
+            msg = 'Pixel with hid: `' + hid + '` has been successfully fired!';
+            hid = data['OldImage']['hid']['N'];
+            userName = 'Happy Bot';
+            iconEmoji = ':stuck_out_tongue_winking_eye:';
         }
-    };
+        msg += '\n Event ID: `' + eventId + '`';
 
-    // request object
-    var req = https.request(options, function (res) {
-      var result = '';
-      res.on('data', function (chunk) {
-        result += chunk;
-      });
-      res.on('end', function () {
-        console.log(result);
-      });
-      res.on('error', function (err) {
-        console.log(err);
-      })
+        // form data
+        var postData = JSON.stringify({
+            channel: config.slackWebHookChannel,
+            username: userName,
+            text: msg,
+            icon_emoji: iconEmoji
+        });
+
+        // request option
+        var options = {
+            host: config.slackWebHookHost,
+            port: 443,
+            method: 'POST',
+            path: config.slackWebHookPath,
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": Buffer.byteLength(postData)
+            }
+        };
+
+        // request object
+        var req = https.request(options, function (res) {
+          var result = '';
+          res.on('data', function (chunk) {
+            result += chunk;
+          });
+          res.on('end', function () {
+            console.log(result);
+          });
+          res.on('error', function (err) {
+            console.log(err);
+          })
+        });
+
+        // req error
+        req.on('error', function (err) {
+          console.log(err);
+        });
+
+        //send request witht the postData form
+        req.write(postData);
+        req.end();
     });
 
-    // req error
-    req.on('error', function (err) {
-      console.log(err);
-    });
-
-    //send request witht the postData form
-    req.write(postData);
-    req.end();
+    log("Successfully processed " + event.Records.length + " records.");
 })
