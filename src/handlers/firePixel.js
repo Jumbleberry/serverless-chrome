326dd07fce177fa3c7204c596996bef9511aa7be
Hao <linghaoo@gmail.com>
2017-08-31 11:28:12 -0400

    Reduce lambda memory size and log all requests of headless chrome

--- src/handlers/firePixel.js
+++ src/handlers/firePixel.js
@@ -48,7 +48,7 @@ export default (async function firePixelHandler (event) {
   log('Web socket connection closed.')
 
   deleteFromTable(event)
-  // log('firePixelHandler result:', JSON.stringify(requestsMade, null, ' '))
+  log('Requests made by headless chrome:', JSON.stringify(requestsMade, null, ' '))
 
   return {
     statusCode: 200,
