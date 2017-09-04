27c70e4639dd8e859394f4957437d6e8f258d260
Hao <linghaoo@gmail.com>
2017-09-04 16:38:28 -0400

    Reduce chrome page load time from 60 to 15

--- src/config.js
+++ src/config.js
@@ -34,7 +34,7 @@ const defaultChromeFlags = [
 ]
 
 const defaultChromeConfig = {
-  pageLoadTimeout: 1000 * 60, // Give the page max 60 seconds to load. time is money!
+  pageLoadTimeout: 1000 * 15, // Give the page max 15 seconds to load. time is money!
 }
 
 export default {
